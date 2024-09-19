fd75f210e48e514a2d143ad6b45fc3e3faff3aba
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-09-19 18:29:15 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import './App.css';
+import HeaderOne from "./HeaderOne.js";
+
+function App() {
+
+  return (
+    <div>
+      <HeaderOne title="Tea Cups For Sale" />
+      <div>
+        <img src={require("./images/blue-tea-cup.jpeg")} alt={"blue tea cup"} />
+        <img src={require("./images/red-tea-cup.jpeg")} alt={"red tea cup"} />
+      </div>
+      <div>
+        <img src={require("./images/green-tea-cup.jpeg")} alt={"green tea cup"} />
+        <img src={require("./images/yellow-tea-cup.jpeg")} alt={"yellow tea cup"} />
+      </div>
+    </div>
+     );
+}
+
+export default App;
---
+++ HeaderOne.js
@@ -0,0 +1,5 @@
+function HeaderOne(props) {
+    return <h1>{props.title}</h1>;
+}
+
+export default HeaderOne;
