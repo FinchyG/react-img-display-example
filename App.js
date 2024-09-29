92ab401c51b8fad686112719a56add5b11448e89
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-09-29 18:42:08 +0100

    img alt attribute correction

--- App.js
+++ App.js
@@ -8,12 +8,12 @@ function App() {
     <div>
       <HeaderOne title="Tea Cups For Sale" />
       <div>
-        <img src={require("./images/blue-tea-cup.jpeg")} alt={"blue tea cup"} />
-        <img src={require("./images/red-tea-cup.jpeg")} alt={"red tea cup"} />
+        <img src={require("./images/blue-tea-cup.jpeg")} alt="blue tea cup" />
+        <img src={require("./images/red-tea-cup.jpeg")} alt="red tea cup" />
       </div>
       <div>
-        <img src={require("./images/green-tea-cup.jpeg")} alt={"green tea cup"} />
-        <img src={require("./images/yellow-tea-cup.jpeg")} alt={"yellow tea cup"} />
+        <img src={require("./images/green-tea-cup.jpeg")} alt="green tea cup" />
+        <img src={require("./images/yellow-tea-cup.jpeg")} alt="yellow tea cup" />
       </div>
     </div>
      );
